8d6fd1f697730d9fb9f823a7bde0d1eeb23f7b35
Tamami Polk <tamami_polk@hotmail.com>
2019-05-11 19:35:27 -0500

    added switch statement instead of if statement

--- liri.js
+++ liri.js
@@ -11,26 +11,48 @@ var searchItem = process.argv.slice(3).join(" ");
 var divider = "\n------------------------------------------------------------\n\n";
 
 
-
 function whatSelection() {
-    if (selection == "concert-this") {
-        getConcert(searchItem);
-    } else if (selection == "spotify-this-song") {
-        if (searchItem === "") {
-            searchItem = "Ace of Base The Sign";
-        }
-        getSpotify(searchItem);
-    } else if (selection == "movie-this") {
-        if (searchItem === "") {
-            searchItem = "Mr. Nobody";
-        }
-        getMovie(searchItem)
-    } else if (selection == "do-what-it-says") {
-        doWhatItSays();
-    } else console.log("Please choose one of the following commands: concert-this, spotify-this-song, or movie-this");
+    switch (selection) {
+        case 'concert-this':
+            getConcert(searchItem);
+            break;
+        case 'movie-this':
+            if (searchItem === "") {
+                searchItem = "Mr. Nobody";
+            }
+            getMovie(searchItem);
+            break;
+        case 'spotify-this-song':
+            if (searchItem === "") {
+                searchItem = "Ace of Base The Sign";
+            }
+            getSpotify(searchItem);
+            break;
+        case 'do-what-it-says':
+            doWhatItSays();
+            break;
+        default:
+            console.log("Please choose one of the following commands: concert-this, spotify-this-song, or movie-this");
+    }
 }
 
+
+//     if (selection == "concert-this") {
+//         getConcert(searchItem);
+//     } else if (selection == "spotify-this-song") {
+//
+//     } else if (selection == "movie-this") {
+//         if (searchItem === "") {
+//             searchItem = "Mr. Nobody";
+//         }
+//         getMovie(searchItem)
+//     } else if (selection == "do-what-it-says") {
+//         doWhatItSays();
+//     } else console.log("Please choose one of the following commands: concert-this, spotify-this-song, or movie-this");
+// }
+
 whatSelection();
+
 // pass in the band or artist and call API to get dates
 function getConcert(searchItem) {
     console.log("Concert function");
@@ -39,13 +61,14 @@ function getConcert(searchItem) {
     axios.get(URL).then(function (response) {
         var jsonData = response.data;
 
-            jsonData.forEach(function (entry) {
-                var showData =
-                    "Venue: " + entry.venue.name +
-                    " Location: " + entry.venue.city + ", " + entry.venue.country +
-                    "Date: " + moment(entry.datetime).format("MM/DD/YYYY");
-                writeToFile(showData);
-            })
+        jsonData.forEach(function (entry) {
+            var showData =
+                "Venue: " + entry.venue.name +
+                " Location: " + entry.venue.city + ", " + entry.venue.country +
+                "Date: " + moment(entry.datetime).format("MM/DD/YYYY");
+            console.log(showData);
+            writeToFile(showData);
+        })
     })
 }
 
@@ -53,13 +76,14 @@ function getConcert(searchItem) {
 function getSpotify(searchItem) {
     console.log("Spotify function");
 
-    spotify.search({ type: 'track', query: searchItem, limit: 1 }, function(err, data) {
+    spotify.search({type: 'track', query: searchItem, limit: 1}, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
 
         var showData = "Artist: " + data.tracks.items[0].artists[0].name + "\n" + "Song Name: " + data.tracks.items[0].name + "\n" + "Song Preview: " + data.tracks.items[0].preview_url + "\n" +
             "Album: " + data.tracks.items[0].album.name;
+        console.log(showData);
 
         writeToFile(showData);
 
@@ -72,7 +96,7 @@ function getMovie(searchItem) {
 
     var URL = "https://www.omdbapi.com/?t=" + searchItem + "&y=&plot=short&apikey=trilogy";
 
-    axios.get(URL).then( function (response) {
+    axios.get(URL).then(function (response) {
         var jsonData = response.data;
 
         var showData = "Movie Title: " + jsonData.Title + "\n" +
@@ -83,6 +107,7 @@ function getMovie(searchItem) {
             "Language: " + jsonData.Language + "\n" +
             "Plot: " + jsonData.Plot + "\n" +
             "Actors: " + jsonData.Actors + "\n";
+        console.log(showData);
 
         writeToFile(showData);
 
@@ -90,9 +115,8 @@ function getMovie(searchItem) {
 
 }
 
-function doWhatItSays()
-{
-    fs.readFile('random.txt', "utf8", function(err, response) {
+function doWhatItSays() {
+    fs.readFile('random.txt', "utf8", function (err, response) {
         console.log("File " + response);
         var randomArray = response.toString().split(",");
         selection = randomArray[0];
@@ -103,7 +127,7 @@ function doWhatItSays()
 }
 
 function writeToFile(showData) {
-    fs.appendFile("log.txt", showData + divider, function(err) {
+    fs.appendFile("log.txt", showData + divider, function (err) {
         if (err) throw err;
     });
 }
